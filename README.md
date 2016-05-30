2ec284add829c694fe4f274385b182737bf6b6d4
Anastassios Martakos <amartako@phys.ethz.ch>
2016-05-30 14:14:00 +0200

    Added license information and README

---
+++ README.md
@@ -0,0 +1,12 @@
+Snake
+=====
+This is an implementation of the popular game Snake in python.
+
+Controls
+--------
+Use the arrows on your keyboard to control the snake.
+
+Requirements
+------------
+  - python3
+  - tkinter (python-tk)
--- lib/snake.py
+++ lib/snake.py
@@ -1,3 +1,18 @@
+'''
+Created by: Anastassios Martakos
+
+Copyright 2016 Anastassios Martakos
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+'''
+
 import tkinter as tk
 import lib.models as models
 from lib.models import SnakeObj, Movement, Food
