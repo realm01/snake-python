f88d6a8d9d4913f477618312065eed82b6101c56
Anastassios Martakos <amartako@phys.ethz.ch>
2016-05-30 14:03:29 +0200

    Snake is working, needs finetuning in food spawning

--- lib/models.py
+++ lib/models.py
@@ -16,9 +16,9 @@ class Movement():
         self.direction = self.LEFT
         self.last_direction = self.LEFT
 
-    def updateAllDir(self, vec):
-        self.direction = vec if Movement.getOpposite(vec) != self.direction else self.direction
-        self.last_direction = vec if Movement.getOpposite(vec) != self.direction else self.direction
+    def updateAllDir(self, vec, prev):
+        self.direction = vec if Movement.getOpposite(vec) != self.direction and Movement.getOpposite(vec) != prev.last_direction else self.direction
+        self.last_direction = vec if Movement.getOpposite(vec) != self.direction and Movement.getOpposite(vec) != prev.last_direction else self.direction
 
     @staticmethod
     def getOpposite(vec):
@@ -26,9 +26,10 @@ class Movement():
 
 
 class Mesh(Base):
-    def __init__(self, scale=1, pos=Vector2(0, 0), mesh=[]):
+    def __init__(self, scale=1, pos=Vector2(0, 0), mesh=[], color = "black"):
         Base.__init__(self, scale, pos)
         self.mesh = mesh
+        self.color = color
 
     def render(self, canvas, modifier=Vector2(0, 0)):
         tmp_mesh = list(self.mesh)
@@ -40,17 +41,17 @@ class Mesh(Base):
             else:
                 tmp_mesh[i] = tmp_mesh[i] * self.scale + self.pos.y * self.scale + modifier.y * self.scale
                 x = True
-        canvas.create_polygon(tmp_mesh, outline="green")
+        canvas.create_polygon(tmp_mesh, fill=self.color)
 
 
 class Block(Mesh, Movement):
-    def __init__(self, scale=1, pos=Vector2(0, 0)):
-        Mesh.__init__(self, scale, pos, [0, 0,  0, 1,  1, 1,  1, 0])
+    def __init__(self, scale=1, pos=Vector2(0, 0), color="black"):
+        Mesh.__init__(self, scale, pos, [0, 0,  0, 1,  1, 1,  1, 0], color)
         Movement.__init__(self)
 
 class Food(Block):
-    def __init__(self, w, h, scale=1):
-        Block.__init__(self, scale, Vector2(0, 0))
+    def __init__(self, w, h, scale=1, color="red"):
+        Block.__init__(self, scale, Vector2(0, 0), color)
         self.w = w
         self.h = h
         self.randomize()
@@ -61,15 +62,16 @@ class Food(Block):
         self.pos = Vector2(randint(0, local_w), randint(0, local_h))
 
 class SnakeObj(Base):
-    def __init__(self, scale=1, pos=Vector2(0, 0)):
+    def __init__(self, scale=1, pos=Vector2(0, 0), color="green"):
         Base.__init__(self, scale, pos)
         self.blocks = []
+        self.color = color
 
     def addBlocks(self, count):
         for i in range(count):
             last_block = self.blocks[len(self.blocks) - 1] if len(self.blocks) - 1 != -1 else Block(1, Vector2(-1, 0))
 
-            self.blocks.append(Block(self.scale, Vector2(last_block.pos.x - last_block.direction.x, last_block.pos.y - last_block.direction.y)))
+            self.blocks.append(Block(self.scale, Vector2(last_block.pos.x - last_block.direction.x, last_block.pos.y - last_block.direction.y), self.color))
             self.blocks[len(self.blocks) - 1].last_direction = last_block.direction
 
     def render(self, canvas):
@@ -77,7 +79,7 @@ class SnakeObj(Base):
             block.render(canvas, self.pos)
 
     def getScore(self):
-        return len(self.blocks) - 3
+        return len(self.blocks) - 3 if len(self.blocks) >= 3 else 0
 
     def move(self, w, h):
         local_w = int(w / self.scale) - 1
--- lib/snake.py
+++ lib/snake.py
@@ -6,64 +6,102 @@ from lib.vector import Vector2
 import time
 
 class Application(tk.Frame):
+    STARTED = 0
+    PAUSED = 1
+    MAINMENU = 2
+
     def moveUp(self, event):
-        self.my_snake.blocks[0].updateAllDir(Movement.UP)
+        if self.game_stat != self.MAINMENU:
+            self.my_snake.blocks[0].updateAllDir(Movement.UP, self.my_snake.blocks[1])
 
     def moveDown(self, event):
-        self.my_snake.blocks[0].updateAllDir(Movement.DOWN)
+        if self.game_stat != self.MAINMENU:
+            self.my_snake.blocks[0].updateAllDir(Movement.DOWN, self.my_snake.blocks[1])
 
     def moveRight(self, event):
-        self.my_snake.blocks[0].updateAllDir(Movement.RIGHT)
+        if self.game_stat != self.MAINMENU:
+            self.my_snake.blocks[0].updateAllDir(Movement.RIGHT, self.my_snake.blocks[1])
 
     def moveLeft(self, event):
-        self.my_snake.blocks[0].updateAllDir(Movement.LEFT)
+        if self.game_stat != self.MAINMENU:
+            self.my_snake.blocks[0].updateAllDir(Movement.LEFT, self.my_snake.blocks[1])
+
+    def startGame(self, event):
+        if self.game_stat == self.MAINMENU:
+            self.restart()
 
     def __init__(self, master=None):
         self.w=400
         self.h=400
         self.scale = 20
+        self.start_speed = 0.14
+        self.speed = self.start_speed
 
         tk.Frame.__init__(self, master)
+        self.game_stat = self.MAINMENU
         self.master.minsize(width=self.w, height=self.h)
         self.master.maxsize(width=self.w, height=self.h)
         self.canvas = tk.Canvas(master, height=self.h, width=self.w)
         self.canvas.pack()
-        self.restart()
-        self.renderAll()
+        self.my_snake = SnakeObj(self.scale)
+        self.displayStart()
 
         self.master.bind('<Left>', self.moveLeft)
         self.master.bind('<Right>', self.moveRight)
         self.master.bind('<Up>', self.moveUp)
         self.master.bind('<Down>', self.moveDown)
 
+        self.master.bind('<space>', self.startGame)
+
     def moveAll(self):
+        if self.game_stat != self.STARTED:
+            return 0
         self.my_snake.move(self.w, self.h)
 
     def calcCollision(self):
+        if self.game_stat != self.STARTED:
+            return 0
+
         if(self.my_snake.blocks[0].pos == self.food.pos):
+            self.speed *= 0.96
             self.my_snake.addBlocks(1)
             self.food.randomize()
 
         for block in self.my_snake.blocks:
             if(self.my_snake.blocks[0].pos == block.pos and self.my_snake.blocks[0] is not block):
-                self.restart()
+                self.canvas.delete("all")
+                self.canvas.create_text(self.w/2, self.h/3, font=("Arial", 15), text="Game Over")
+                self.displayStart()
 
     def renderAll(self):
+        if self.game_stat == self.MAINMENU:
+            return 0
         self.canvas.delete("all")
         self.my_snake.render(self.canvas)
         self.food.render(self.canvas)
 
+        self.canvas.create_text(30, 10, text="Score: " + str(self.my_snake.getScore()))
+
     def loop(self):
         while(True):
-            self.master.update_idletasks()
-            self.master.update()
             self.moveAll()
             self.calcCollision()
             self.renderAll()
-            time.sleep(0.14)
+            waittime = 200
+            for i in range(waittime):
+                time.sleep(self.speed / waittime)
+                self.master.update_idletasks()
+                self.master.update()
 
+
+    def displayStart(self):
+        self.game_stat = self.MAINMENU
+        self.canvas.create_text(self.w /2, self.h/2, font=("Arial", 20), text="Press Space to start")
+        self.canvas.create_text(self.w /2, self.h - self.h/2.4, font=("Arial", 15), text="Score: " + str(self.my_snake.getScore()))
 
     def restart(self):
+        self.game_stat = self.STARTED
+        self.speed = self.start_speed
         self.my_snake = SnakeObj(self.scale)
         self.my_snake.addBlocks(3)
         self.food = Food(self.w, self.h, self.scale)
